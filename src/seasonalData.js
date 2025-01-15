c7da34833da4d3632b08600aecbf17dd52b4aabd
solux <lee.yeji0327@gmail.com>
2025-01-15 23:49:48 +0900

    feat: seasonalData-contactInfo 작성

--- src/seasonalData.js
+++ src/seasonalData.js
@@ -1,1 +1,7 @@
 export const generation = "13기";
+
+export const contactInfo = {
+  instagram: "https://www.instagram.com/sookmyung_likelion/",
+  kakaotalk: "https://open.kakao.com/me/likelion_sookmyung",
+  email: "https://mail.google.com/mail/?view=cm&fs=1&to=sookmyung@likelion.org",
+};
