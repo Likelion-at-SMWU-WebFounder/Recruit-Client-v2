444a4bd361e3b030ea944c2a104b88d81ea9003c
solux <lee.yeji0327@gmail.com>
2025-01-19 16:13:10 +0900

    comment: seasonalData 설명 주석 추가

--- src/seasonalData.js
+++ src/seasonalData.js
@@ -1,15 +1,18 @@
-export const generation = "13기";
+// ABOUTUS 탭 - 지원하기 버튼 텍스트 (AboutUs 컴포넌트)
+export const generation = "13기"; // 모집 기수
 
+// CONTACT 탭 - 문의처 링크 (Contact 컴포넌트)
 export const contactInfo = {
-  instagram: "https://www.instagram.com/sookmyung_likelion/",
-  kakaotalk: "https://open.kakao.com/me/likelion_sookmyung",
-  email: "https://mail.google.com/mail/?view=cm&fs=1&to=sookmyung@likelion.org",
+  instagram: "https://www.instagram.com/sookmyung_likelion/", // 인스타그램 링크
+  kakaotalk: "https://open.kakao.com/me/likelion_sookmyung", // 카카오톡 오픈채팅 링크
+  email: "https://mail.google.com/mail/?view=cm&fs=1&to=sookmyung@likelion.org", // email 작성 링크
 };
 
+//
 export const notionUrl =
   "https://tattered-cabinet-6cd.notion.site/12-at-Sookmyung-db2f25f1f35c48a6b068dbc5e33577e4?pvs=4";
 
-// Recruit 탭 - 모집 일정 (Information 컴포넌트)
+// RECRUIT 탭 - 모집 일정 (Information 컴포넌트)
 export const recruitInfo = {
   docSubmissionDate: "2/17(토)~2/23(금) 18:00", // 서류 접수 일정
   passAnnounce_1st: "2/27(화)", // 1차 합격자 발표
