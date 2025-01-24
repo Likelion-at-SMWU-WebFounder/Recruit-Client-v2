1c0ac7718d8a4dc151aca6db5a7105acda110f2e
solux <lee.yeji0327@gmail.com>
2025-01-24 19:42:04 +0900

    refactor: imgg 오타가 아니었음..

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -74,7 +74,7 @@ const NotFound = () => {
             src={`${process.env.REACT_APP_IMAGE_URL}/lionwow.svg`}
             alt="lionwow"
           />
-          <Img
+          <Imgg
             src={`${process.env.REACT_APP_IMAGE_URL}/question.svg`}
             alt="question"
           />
