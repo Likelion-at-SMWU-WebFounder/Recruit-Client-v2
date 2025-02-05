57d9022a7857bbde778f3d77404de644549d2677
solux <lee.yeji0327@gmail.com>
2025-02-06 00:55:56 +0900

    chore: 미디어쿼리 768 -> 1250 revert

--- src/MediaQuery/useMediaQuery.js
+++ src/MediaQuery/useMediaQuery.js
@@ -2,12 +2,12 @@ import { useMediaQuery } from "react-responsive";
 
 export const Desktop = ({ children }) => {
   const isDesktop = useMediaQuery({
-    minWidth: 769,
+    minWidth: 1251,
   });
   return isDesktop ? children : null;
 };
 
 export const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 1250 });
   return isMobile ? children : null;
 };
