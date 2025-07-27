eec881c21476298e35b882ed1f0ec1651dbc7588
earl9rey <earl9rey@naver.com>
2025-07-27 16:50:18 +0900

    refactor: 메이커스 1기 데이터 분리

---
+++ src/data/makers/1st.js
@@ -0,0 +1,58 @@
+// 1기 홈페이지 개발자들 데이터 배열 
+export const firstMakerData = [
+    {
+      generation: 1, // 1기
+      members: [
+        {
+          name: "소희수",
+          part: "기획/디자인",
+          img: `${process.env.REACT_APP_IMAGE_URL}/heesu.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 중어중문학부 • IT공학전공",
+        },
+        {
+          name: "나유진",
+          part: "프론트엔드",
+          img: `${process.env.REACT_APP_IMAGE_URL}/yujin.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 컴퓨터과학전공",
+        },
+        {
+          name: "배지윤",
+          part: "프론트엔드",
+          img: `${process.env.REACT_APP_IMAGE_URL}/jiyoon.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 수학과 • IT공학전공",
+        },
+        {
+          name: "양은수",
+          part: "프론트엔드",
+          img: `${process.env.REACT_APP_IMAGE_URL}/eunsu.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 컴퓨터과학전공",
+        },
+        {
+          name: "이수민",
+          part: "백엔드",
+          img: `${process.env.REACT_APP_IMAGE_URL}/sumin.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 컴퓨터과학전공",
+        },
+        {
+          name: "임아현",
+          part: "백엔드",
+          img: `${process.env.REACT_APP_IMAGE_URL}/ahyun.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 IT공학전공",
+        },
+        {
+          name: "박소윤",
+          part: "백엔드",
+          img: `${process.env.REACT_APP_IMAGE_URL}/soyoon.svg`,
+          sooklion: "멋쟁이사자처럼 숙명여대 10기",
+          major: "숙명여대 IT공학전공",
+        },
+      ],
+    },
+];
+  
