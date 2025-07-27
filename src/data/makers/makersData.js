e91d9acd5d87f9731e54aa0e7a4824fb94f1b813
earl9rey <earl9rey@naver.com>
2025-07-27 16:50:50 +0900

    refactor: 기수별 메이커스 데이터 통합

---
+++ src/data/makers/makersData.js
@@ -0,0 +1,9 @@
+// 홈페이지 개발자들 데이터 배열 (Makers 컴포넌트 - Footer 통해 진입)
+
+import { firstMakerData } from "./1st";
+import { secondMakerData } from "./2nd";
+
+export const makers = [
+    ...firstMakerData,
+    ...secondMakerData,
+];
